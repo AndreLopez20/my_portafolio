473f17481e27e0e0f5c000a3823ba723d1209e9c
AndreLopez20 <lopez.eduandre@gmail.com>
2024-06-14 23:15:41 -0600

    Avance 2  Portfolio

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const phrases = [
+    "< Web Developer />",
+    "< Software Engineer />",
+];
+
+let currentPhraseIndex = 0;
+let currentCharIndex = 0;
+const typingSpeed = 100; // Milisegundos entre caracteres
+const deletingSpeed = 50; // Milisegundos entre caracteres al borrar
+const delayBetweenPhrases = 2000; // Milisegundos de espera entre frases
+const typewriterTextElement = document.getElementById("typewriter-text");
+
+function typePhrase() {
+    if (currentCharIndex < phrases[currentPhraseIndex].length) {
+        typewriterTextElement.textContent += phrases[currentPhraseIndex].charAt(currentCharIndex);
+        currentCharIndex++;
+        setTimeout(typePhrase, typingSpeed);
+    } else {
+        setTimeout(deletePhrase, delayBetweenPhrases);
+    }
+}
+
+function deletePhrase() {
+    if (currentCharIndex > 0) {
+        typewriterTextElement.textContent = phrases[currentPhraseIndex].substring(0, currentCharIndex - 1);
+        currentCharIndex--;
+        setTimeout(deletePhrase, deletingSpeed);
+    } else {
+        currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+        setTimeout(typePhrase, typingSpeed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    typePhrase();
+});
+
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
